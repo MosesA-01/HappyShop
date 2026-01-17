8ed915717aafb6523ede655e1ac4abbc674e589d
Moses <moagbeey@gmail.com>
2026-01-17 19:13:02 +0000

    Add initial JUnit smoke test

---
+++ src/test/java/ci553/happyshop/SmokeTest.java
@@ -0,0 +1,11 @@
+package ci553.happyshop;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+    @Test
+    void smokeTest() {
+        assertTrue(true);
+    }
+}
